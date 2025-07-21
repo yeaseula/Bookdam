cbfa32c254e629c13b1ac6fa94ce70cae9c3646d
yeaseula <kminea@naver.com>
2025-07-21 23:34:00 +0900

    🚀 Navigation, done에 lottie 추가, js 파일 및 navigation 파일 분리
    lottie 추가된 페이지에는 애니메이션 효과가 추가됐습니다. head 부분 코드 일부 정리했습니다.

---
+++ js/common.js
@@ -0,0 +1,63 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetch('./navigation.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('bottom_navigator').innerHTML = data;
+            const animations = {
+                home: lottie.loadAnimation({
+                    container: document.getElementById('home-icon'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: (currentpage === 'home'),
+                    path: 'nav-animation/home.json'
+                }),
+                search: lottie.loadAnimation({
+                    container: document.getElementById('review-icon'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: (currentpage === 'review'),
+                    path: 'nav-animation/review.json'
+                }),
+                review: lottie.loadAnimation({
+                    container: document.getElementById('reading-icon'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: (currentpage === 'reading'),
+                    path: 'nav-animation/review2.json'
+                }),
+                mypage: lottie.loadAnimation({
+                    container: document.getElementById('mypage-icon'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: (currentpage === 'mypage'),
+                    path: 'nav-animation/profile.json',
+                }),
+            };
+
+            if(currentpage !== 'mypage') {
+                animations.mypage.goToAndStop(80,true)
+            }
+        })
+        .catch(error => {
+        console.error('파일 로딩 오류:', error);
+    });
+
+    //footer my information toggle
+    const Copytogglebtn = $('#footer-toggle');
+
+    Copytogglebtn.addEventListener('click',()=>{
+        const Copytogglecont = $('.copyright-dropdown');
+        const hasCont = Copytogglecont.classList.contains('active');
+
+        if(!hasCont) {
+            Copytogglecont.classList.add('active');
+        } else {
+            Copytogglecont.classList.remove('active');
+        }
+    })
+
+
+
+})
--- js/page.js
+++ js/page.js
@@ -1,78 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const $ = (node) => document.querySelector(node);
-    
 
-    //main slider
-    const MyBookList = new Swiper('.my_book_list', {
-        slidesPerView: 'auto',
-        loop: true,
-        centeredSlides : true,
-    });
-
-    //main slider2
-    const MyRecomandBoook = new Swiper('.my_recomand_book', {
-        slidesPerView: 1.15,
-        loop: true,
-        spaceBetween:15,
-
-    });
-
-
-    //calendar for book stamp
-    var calendarEl = document.getElementById('calendar');
-    var initialLocaleCode = 'en';
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-            left:'',
-            right: 'prev,next',
-            center: 'title',
-        },
-        locale:initialLocaleCode,
-    
-    });
-    calendar.render();
-
-    //data 연결 예정입니다
-    //기록 날짜를 불러와 캘린더 객체의 date키 값과 일치하는지 확인한 후 스탬프를 찍습니다.
-    let stampDates = ['2025-07-10', '2025-07-12', '2025-07-19'];
-    function StampFunc () {
-        
-        let calendarbox = document.querySelectorAll('.fc-scrollgrid-sync-table td');
-        let dayList = [...Array.from(calendarbox)];
-        dayList.forEach((ele,idx)=>{
-            let {date:dayValue} =dayList[idx].dataset
-            stampDates.forEach((ele,idx2)=>{
-                if (dayValue == stampDates[idx2]) {
-                    dayList[idx].classList.add('simple')
-                } 
-            })
-        })
-    }
-
-    window.onload = function(){
-        StampFunc();
-        const CalPrevBtn = document.querySelector('.fc-prev-button')
-        const CalNextBtn = document.querySelector('.fc-next-button')
-
-        CalPrevBtn.addEventListener('click',()=>{StampFunc()})
-        CalNextBtn.addEventListener('click',()=>{StampFunc()})
-    }
-
-
-    //footer my information toggle
-    const Copytogglebtn = $('#footer-toggle');
-
-    Copytogglebtn.addEventListener('click',()=>{
-        const Copytogglecont = $('.copyright-dropdown');
-        const hasCont = Copytogglecont.classList.contains('active');
-
-        if(!hasCont) {
-            Copytogglecont.classList.add('active');
-        } else {
-            Copytogglecont.classList.remove('active');
-        }
-    })
+    const animations = {
+        done: lottie.loadAnimation({
+            container: document.getElementById('lottie-animation'),
+            renderer: 'svg',
+            loop: false,
+            autoplay: (currentpage === 'done'),
+            path: 'nav-animation/Celebrations.json',
+        }),
+    };
 
 });
 
