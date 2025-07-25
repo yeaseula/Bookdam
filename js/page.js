2c46fda6552689c8f9ecf659f0e4e5f96bbdec0c
yeaseula <kminea@naver.com>
2025-07-26 00:15:07 +0900

    🐛 Done.html 관련 js 파일 삭제 복구
    +간단한 css 문제 수정

--- js/page.js
+++ js/page.js
@@ -428,6 +428,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         loadReviews();
     }
-
+    if(currentpage == 'done') {
+        const animations = {
+            done: lottie.loadAnimation({
+                container: document.getElementById('lottie-animation'),
+                renderer: 'svg',
+                loop: false,
+                autoplay: (currentpage === 'done'),
+                path: 'nav-animation/Celebrations.json'
+            }),
+        };
+    }
 });
 
