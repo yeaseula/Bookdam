a4589ca30f828937766ae7f211c082c46e4c9025
yeaseula <kminea@naver.com>
2025-07-27 22:16:38 +0900

    main slider와 review list 연결

--- js/main.js
+++ js/main.js
@@ -4,17 +4,42 @@ document.addEventListener('DOMContentLoaded', function () {
     const sheetId = '1EiQpFub62jL2VZOULwozl_f3hkz1J-wVec9rcSbR5CU';
     const gid = '1116419757';
 
-    //main slider
+
+    const MyReviewThumb = [];
+
     const MyBookList = new Swiper('.my-book-list', {
         slidesPerView: 'auto',
         loop: true,
         centeredSlides : true,
+        autoplay:true
+        //a11y: false,
     });
 
+    //kakao api로 북커버 호출 함수 (공통)
+    async function fetchMyReviewCover(title,author) {
+        try {
+            const query = `${title} ${author}`;
+            const apiUrl = `https://dapi.kakao.com/v3/search/book?target=all&query=${encodeURIComponent(query)}`;
 
+            const res = await fetch(apiUrl, {
+                headers: {
+                    Authorization: `KakaoAK ${kakaoKey}`
+                }
+            });
+            const data = await res.json();
+            const filtered = data.documents.find(book =>
+                book.title.includes(title) && book.authors.join(',').includes(author)
+            );
 
+            return (filtered || data.documents[0])?.thumbnail || '';
 
-    //calendar for book stamp
+            } catch (e) {
+                console.error('카카오 API 오류:', e);
+                return '';
+            }
+    }
+
+    //calendar for book stamp!
     var calendarEl = document.getElementById('calendar');
     var initialLocaleCode = 'en';
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -28,10 +53,18 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     calendar.render();
 
-    //data 연결 예정입니다
     //기록 날짜를 불러와 캘린더 객체의 date키 값과 일치하는지 확인한 후 스탬프를 찍습니다.
-
     const stampDates = [];
+
+    //google 시트의 날짜 형식은 Date(2025, 5, 10) 형태의 문자열로 출력, 변환처리 필요
+    function sheetDateFormat(rawDate) {
+        const dateMatch = rawDate.match(/Date\((\d+),\s*(\d+),\s*(\d+)\)/);
+        if (!dateMatch) return rawDate;
+            const year = dateMatch[1];
+            const month = String(Number(dateMatch[2]) + 1).padStart(2, '0'); // 0부터 시작
+            const day = String(dateMatch[3]).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+    }
 
     async function loadReviewDetail() {
         const url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json&gid=${gid}`;
@@ -46,28 +79,38 @@ document.addEventListener('DOMContentLoaded', function () {
         const rows = json.table.rows;
 
         for(const row of rows) {
-            //google 시트의 날짜 형식은 Date(2025, 5, 10) 형태의 문자열로 출력, 변환처리 필요
-            function sheetDateFormat(rawDate) {
-                const dateMatch = rawDate.match(/Date\((\d+),\s*(\d+),\s*(\d+)\)/);
-                if (!dateMatch) return rawDate;
-                    const year = dateMatch[1];
-                    const month = String(Number(dateMatch[2]) + 1).padStart(2, '0'); // 0부터 시작
-                    const day = String(dateMatch[3]).padStart(2, '0');
-                    return `${year}-${month}-${day}`;
-                }
-            const startDateRaw = row.c[4]?.v || '';
+            //캘린더에 필요한 데이터
             const endDateRaw = row.c[5]?.v || '';
 
             // 날짜 형식 변환
             const endDate = sheetDateFormat(endDateRaw);
-            stampDates.push(endDate)
+            stampDates.push(endDate);
+
+            //메인 슬라이드에 필요한 데이터
+            const title = row.c[2]?.v || '';
+            const author = row.c[3]?.v || '';
+
+            const thumb = await fetchMyReviewCover(title,author);
+            MyReviewThumb.unshift(thumb);
         }
         StampFunc();
         readHistory();
+        MainSlideThumb();
     }
 
     loadReviewDetail();
 
+    function MainSlideThumb(){
+        //기본 슬라이드 10장
+        const slideContainer = MyBookList.slides;
+
+        console.log(slideContainer)
+        slideContainer.forEach((ele)=>{
+            const {swiperSlideIndex:targetindex} = ele.dataset;
+            const index = Number(targetindex);
+            ele.style.backgroundImage = `url('${MyReviewThumb[index]}')`;
+        })
+    }
 
     function StampFunc () {
         let calendarbox = document.querySelectorAll('.fc-scrollgrid-sync-table td');
@@ -93,7 +136,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //카카오 api 불러오기
 
-    const keywords = ['국내소설', '베스트셀러', '해외소설', '소설', '에세이'];
+    const keywords = ['국내소설', '해외소설', '소설', '인기'];
     const slideCount = 7;
 
     //api로 데이터 다운
