1bcabd50343dcc66eeed6415bddf4372c9a27281
yeaseula <kminea@naver.com>
2025-08-10 00:17:52 +0900

    add build script and copy static files to dist

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'js/main.js',       // 커스텀 JS 진입점
@@ -12,5 +13,21 @@ export default {
     sourcemap: true
   },
   treeshake : true,
-  plugins: [resolve(), commonjs(), terser(),  visualizer({ filename: './dist/stats.html' }) ]
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser(),
+    visualizer({ filename: './dist/stats.html' }),
+    copy({
+      targets: [
+        { src: '*.html', dest: 'dist' },        // index.html + 서브페이지들
+        { src: 'css/**/*', dest: 'dist/css' },  // css 전체
+        { src: 'assets/**/*', dest: 'dist/assets' }, // 이미지, 폰트 등
+        { src: 'fonts/**/*', dest: 'dist/fonts'},
+        { src: 'js/**/*', dest: 'dist/js' },
+        { src: 'nav-animation', dest: 'dist/nav-animation'}
+      ],
+      verbose: true
+    })
+  ]
 };
